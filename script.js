69e10ff698ac84b8c69e69763443ac03681760b1
M. Montgomery <mmontgomery@protonmail.com>
2018-08-25 20:56:49 -0400

    Added photo slideshow.
    Set up brief slideshow of photos on homepage, automatically switching between them.

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ for (var i = 0; i < headerList.length; i++) {
     };
 }
 
+// set up photo slideshow
+var photoIndex = 0;
+var photos = document.getElementsByClassName("slideshow");
+function slideshow () {
+
+    // hide previous photo and update index
+    photos[photoIndex].style.display = "none";
+    photoIndex = (photoIndex + 1) % photos.length;
+
+    // show next photo
+    photos[photoIndex].style.display = "inline";
+
+    // pause to display photo
+    setTimeout(slideshow, 5000);
+
+}
+slideshow();

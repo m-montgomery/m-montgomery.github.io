6c8396697f562bdf4ee96238ef18e05c30e07364
M. Montgomery <mmontgomery@protonmail.com>
2018-11-06 14:36:59 -0500

    Modified file structure.
    Separated scripts into different files; updated page files to match.

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+
+// define header link titles
+var titles = {
+    "home"     : "This site's homepage",
+    "about"    : "Get to know me better!",
+    "resume"   : "Download my resume here",
+    "projects" : "Example coding projects"
+}
+
+// set hover actions for header elements
+var headerList = document.getElementsByClassName("header-text");
+for (var i = 0; i < headerList.length; i++) {
+    
+    // add action to link
+    headerList[i].onmouseover = function() {
+	if (window.location.href == this.href)
+	    this.setAttribute("title", "You're already here!");
+	else 
+	    this.setAttribute("title", titles[this.id]);
+    };
+}
